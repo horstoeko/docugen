3a2879c2dc1a2cf243795b6488c31e8a6fb5cf76
Daniel Erling <daniel.erling@gp-papenburg.de>
2025-04-02 09:34:26 +0200

    [ENH] Added support for custom Outputters

--- examples/GenerateDocumentation.php
+++ examples/GenerateDocumentation.php
@@ -2,22 +2,33 @@
 
 use horstoeko\docugen\DocGenerator;
 use horstoeko\docugen\DocGeneratorConfig;
+use horstoeko\docugen\block\DocGeneratorBlockCode;
+use horstoeko\docugen\block\DocGeneratorBlockBlank;
+use horstoeko\docugen\block\DocGeneratorBlockComment;
+use horstoeko\docugen\output\DocGeneratorOutputAbstract;
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+class CustomOutputter extends DocGeneratorOutputAbstract
+{
+    protected function renderCommentBlock(DocGeneratorBlockComment $docGeneratorBlockComment): void
+    {
+        $this->getDocGeneratorOutputBuffer()->addLinesToOutputBuffer($docGeneratorBlockComment->getRenderedLines());
+    }
+
+    protected function renderCodeBlock(DocGeneratorBlockCode $docGeneratorBlockCode): void
+    {
+        $this->getDocGeneratorOutputBuffer()->addLineToOutputBuffer(sprintf('```%s', $docGeneratorBlockCode->getDocGeneratorBlockModel()->getLanguage()));
+        $this->getDocGeneratorOutputBuffer()->addLinesToOutputBuffer($docGeneratorBlockCode->getRenderedLines());
+        $this->getDocGeneratorOutputBuffer()->addLineToOutputBuffer('```');
+    }
+
+    protected function renderBlankBlock(DocGeneratorBlockBlank $docGeneratorBlockBlank): void
+    {
+        $this->getDocGeneratorOutputBuffer()->addLinesToOutputBuffer($docGeneratorBlockBlank->getRenderedLines());
+    }
+}
 
 $config = DocGeneratorConfig::loadFromFile(__DIR__ . '/config2.json');
 
 $generator = DocGenerator::factory($config)->build();
-
-/*
-foreach ($generator->getDocumentations() as $documentation) {
-    foreach ($documentation->getBlocks() as $block) {
-        echo get_class($block) . PHP_EOL;
-        foreach($block->getLines() as $line) {
-            echo $line . PHP_EOL;
-        }
-    }
-}
-*/
-
-//print_r($generator->getDocumentationOutputs()[0]->getRenderedLines());
---
+++ src/output/DocGeneratorOutputCustom.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This file is a part of horstoeko/docugen
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace horstoeko\docugen\output;
+
+use horstoeko\docugen\block\DocGeneratorBlockBlank;
+use horstoeko\docugen\block\DocGeneratorBlockCode;
+use horstoeko\docugen\block\DocGeneratorBlockComment;
+use horstoeko\docugen\DocGeneratorOutputBuffer;
+use horstoeko\docugen\output\DocGeneratorOutputAbstract;
+use horstoeko\stringmanagement\StringUtils;
+
+/**
+ * Class representing the HTML outputter
+ *
+ * @category DocuGen
+ * @package  DocuGen
+ * @author   D. Erling <horstoeko@erling.com.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/horstoeko/docugen
+ */
+class DocGeneratorOutputCustom extends DocGeneratorOutputAbstract
+{
+    /**
+     * Instance of the custom output class
+     *
+     * @var DocGeneratorOutputAbstract
+     */
+    protected $docGeneratorOutputCustomInstance;
+
+    /**
+     * @inheritDoc
+     */
+    public function build(): DocGeneratorOutputAbstract
+    {
+        $docGeneratorOutputCustomClassname = $this->getDocGeneratorOutputModel()->getOption('class', '');
+
+        if (StringUtils::stringIsNullOrEmpty($docGeneratorOutputCustomClassname)) {
+            return $this;
+        }
+
+        if (!class_exists($docGeneratorOutputCustomClassname)) {
+            return $this;
+        }
+
+        if (!is_a($docGeneratorOutputCustomClassname, DocGeneratorOutputAbstract::class, true)) {
+            return $this;
+        }
+
+        $this->docGeneratorOutputCustomInstance = $docGeneratorOutputCustomClassname::factory($this->getDocGeneratorOutputBuilder());
+
+        parent::build();
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDocGeneratorOutputBuffer(): DocGeneratorOutputBuffer
+    {
+        return $this->docGeneratorOutputCustomInstance->getDocGeneratorOutputBuffer();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function renderCommentBlock(DocGeneratorBlockComment $docGeneratorBlockComment): void
+    {
+        $this->docGeneratorOutputCustomInstance->renderCommentBlock($docGeneratorBlockComment);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function renderCodeBlock(DocGeneratorBlockCode $docGeneratorBlockCode): void
+    {
+        $this->docGeneratorOutputCustomInstance->renderCodeBlock($docGeneratorBlockCode);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function renderBlankBlock(DocGeneratorBlockBlank $docGeneratorBlockBlank): void
+    {
+        $this->docGeneratorOutputCustomInstance->renderBlankBlock($docGeneratorBlockBlank);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function beforeAllBlocks(): DocGeneratorOutputAbstract
+    {
+        $this->docGeneratorOutputCustomInstance->beforeAllBlocks();
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function afterAllBlocks(): DocGeneratorOutputAbstract
+    {
+        $this->docGeneratorOutputCustomInstance->afterAllBlocks();
+
+        return $this;
+    }
+}
